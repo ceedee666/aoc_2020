856f313bd55f966c1162ded5e737c06d9d4cdb10
Christian Drumm <drumm@fh-aachen.de>
2020-12-10 09:50:03 +0100

    solutions for day 10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from collections import Counter
+from collections import Counter, defaultdict
 from functools import reduce
 import typer
 
@@ -16,16 +16,32 @@ def read_input_file(input_file_path):
     return list(map(lambda l: int(l), lines))
 
 
-def count_jolt_differences(adapters):
+def sort_adapters(adapters):
     adapters.append(0)
     sorted_adapters = sorted(adapters)
     sorted_adapters.append(max(sorted_adapters)+3)
+    return sorted_adapters
 
+
+def count_jolt_differences(adapters):
+    sorted_adapters = sort_adapters(adapters)
     differences = map(
             lambda e: e[1] - sorted_adapters[e[0] - 1],
             enumerate(sorted_adapters[1:], 1))
     
     return Counter(differences)
+
+
+def calculate_possible_combinations(adapters):
+    sorted_adapters = sort_adapters(adapters)
+    combinations = defaultdict(int)
+   
+    combinations[0] = 1
+
+    for i in sorted_adapters[1:]:
+        combinations[i] = combinations[i-1] + combinations[i-2] + combinations[i-3]
+
+    return combinations[max(sorted_adapters)]
 
 
 @app.command()
@@ -36,8 +52,8 @@ def part1(input_file: str):
 
 @app.command()
 def part2(input_file: str):
-    jolt_differences = count_jolt_differences(read_input_file(input_file))
-    print(f"The solution for part 1 is {jolt_differences[1] * jolt_differences[3]}.")
+    combinations = calculate_possible_combinations(read_input_file(input_file))
+    print(f"The solution for part 2 is {combinations}.")
 
 
 if __name__ == "__main__":
