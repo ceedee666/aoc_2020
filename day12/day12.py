98e0bfcacfad5724d72bc7ea5797bd0cb2ebb24c
Christian Drumm <drumm@fh-aachen.de>
2020-12-12 16:01:34 +0100

    solution part2 after 3 hour of mtb :D

--- day12/day12.py
+++ day12/day12.py
@@ -32,15 +32,34 @@ def move(pos, step):
     return new_pos
 
 
-def turn(direction, step):
+def turn_ship(direction, step):
     operation, dist = step
 
     if operation in "RL":
         if operation == "L":
             dist *= -1
         dir_index = DIRECTIONS.index(direction)
-        new_dir_index = (dir_index + dist // 90) % len(DIRECTIONS)
+        new_dir_index = (dir_index + dist // 90) % 4
         return DIRECTIONS[new_dir_index]
+
+
+def turn_waypoint(waypoint, step):
+    operation, dist = step
+    new_waypoint = waypoint
+
+    if operation == "L":
+        dist = 360 - dist
+
+    turn_dir = (dist // 90) % 4
+
+    if turn_dir == 1:
+        new_waypoint = (waypoint[1] * -1, waypoint[0])
+    elif turn_dir == 2:
+        new_waypoint = (waypoint[0] * -1, waypoint[1] * -1)
+    elif turn_dir == 3:
+        new_waypoint = (waypoint[1], waypoint[0] * -1)
+
+    return new_waypoint
 
 
 def move_ship(ship, step):
@@ -54,7 +73,25 @@ def move_ship(ship, step):
         new_ship["dir"] = ship["dir"]
     else:
         new_ship["pos"] = ship["pos"]
-        new_ship["dir"] = turn(ship["dir"], step)
+        new_ship["dir"] = turn_ship(ship["dir"], step)
+
+    return new_ship
+
+
+def move_ship_with_waypoint(ship, step):
+    new_ship = dict()
+
+    if step[0] == "F":
+        new_ship["waypoint"] = ship["waypoint"]
+        new_ship["pos"] = (ship["pos"][0] + step[1] * ship["waypoint"][0],
+                           ship["pos"][1] + step[1] * ship["waypoint"][1])
+    else:
+        if step[0] in DIRECTIONS:
+            new_ship["pos"] = ship["pos"]
+            new_ship["waypoint"] = move(ship["waypoint"], step)
+        else:
+            new_ship["pos"] = ship["pos"]
+            new_ship["waypoint"] = turn_waypoint(ship["waypoint"], step)
 
     return new_ship
 
@@ -63,6 +100,14 @@ def move_ship(ship, step):
 def part1(input_file: str):
     ship = {"pos": (0, 0), "dir": "E"}
     r = reduce(lambda s, p: move_ship(s, p), read_input_file(input_file), ship)
+    distance = abs(r["pos"][0]) + abs(r["pos"][1])
+    print(f"The manhatten distance of the ships position is {distance}.")
+
+
+@app.command()
+def part2(input_file: str):
+    ship = {"pos": (0, 0), "waypoint": (1, 10)}
+    r = reduce(lambda s, p: move_ship_with_waypoint(s, p), read_input_file(input_file), ship)
     distance = abs(r["pos"][0]) + abs(r["pos"][1])
     print(f"The manhatten distance of the ships position is {distance}.")
 
