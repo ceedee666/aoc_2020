7c30d9a3f5ece36f01191ee2ae527ff845b716fa
Christian Drumm <drumm@fh-aachen.de>
2020-12-14 09:41:29 +0100

    solution day 14 part 2

--- day14/day14.py
+++ day14/day14.py
@@ -39,18 +39,43 @@ def apply_bin_mask(mask, val):
     return int(bin_result, 2)
 
 
-def execute_instruction(memory, instruction):
+def apply_bin_mask_v2(mask, val):
+    bin_val = f"{bin(val)[2:]:0>36}"
+    values = [""]
+
+    for i, c in enumerate(mask):
+        new_values = []
+        for v in values:
+            if c == "0":
+                new_values.append(v + bin_val[i])
+            elif c == "1":
+                new_values.append(v + c)
+            else:
+                new_values.append(v + "0")
+                new_values.append(v + "1")
+
+        values = new_values
+
+    return list(map(lambda v: int(v, 2), values))
+
+
+def execute_instruction(memory, instruction, version):
     if instruction["op"] == "mask":
         memory["mask"] = instruction["val"]
-    elif instruction["op"] == "mem":
-        val = apply_bin_mask(memory["mask"], instruction["val"])
-        memory[instruction["add"]] = val
+    else:
+        if version == 1:
+            val = apply_bin_mask(memory["mask"], instruction["val"])
+            memory[instruction["add"]] = val
+        if version == 2:
+            addresses = apply_bin_mask_v2(memory["mask"], instruction["add"])
+            for address in addresses:
+                memory[address] = instruction["val"]
 
     return memory
 
 
-def execute_initialization(program):
-    memory = reduce(lambda m, e: execute_instruction(m, e), program, dict())
+def execute_initialization(program, version=1):
+    memory = reduce(lambda m, e: execute_instruction(m, e, version), program, dict())
     memory.pop("mask")
     return sum(memory.values())
 
@@ -59,6 +84,13 @@ def execute_initialization(program):
 def part1(input_file: str):
     program = read_input_file(input_file)
     result = execute_initialization(program)
+    print(f"The sum of all values in memory is {result}.")
+
+
+@app.command()
+def part2(input_file: str):
+    program = read_input_file(input_file)
+    result = execute_initialization(program, version=2)
     print(f"The sum of all values in memory is {result}.")
 
 
