e9a475493c479aaa49a4e35d9cba8d966f2ca205
Christian Drumm <drumm@fh-aachen.de>
2020-12-26 21:29:24 +0100

    day 24 part 2

--- day24/day24.py
+++ day24/day24.py
@@ -39,12 +39,54 @@ def flip_tile_by_moves(black_tiles, moves):
     return black_tiles
 
 
+def neighbors(tile):
+    return set(map(lambda d: (tile[0] + MOVEMENT[d][0], tile[1] + MOVEMENT[d][1]), MOVEMENT))
+
+
+def all_neighbors(tiles):
+    result = set()
+    for tile in tiles:
+        result |= neighbors(tile)
+    return result
+
+
+def next_state(tiles_to_check, black_tiles):
+    next_black_tiles = set()
+    for tile in tiles_to_check:
+        nbs = neighbors(tile)
+        black_nbs = nbs & black_tiles
+
+        if tile in black_tiles:
+            if len(black_nbs) in [1, 2]:
+                next_black_tiles |= set([tile])
+        else:
+            if len(black_nbs) == 2:
+                next_black_tiles |= set([tile])
+
+    return next_black_tiles
+
+
 @app.command()
 def part1(input_file_path: str):
     all_moves = list(map(lambda l: conv_steps_to_moves(l),
                          read_input_file(input_file_path)))
-    black_tiles = reduce(lambda s, m: flip_tile_by_moves(s, m), all_moves, set())
+    black_tiles = reduce(lambda s, m: flip_tile_by_moves(s, m),
+                         all_moves, set())
     print(f"{len(black_tiles)} tiles are left with the black side up.")
+
+
+@app.command()
+def part2(input_file_path: str):
+    all_moves = list(map(lambda l: conv_steps_to_moves(l),
+                         read_input_file(input_file_path)))
+    black_tiles = reduce(lambda s, m: flip_tile_by_moves(s, m),
+                         all_moves, set())
+
+    for _ in range(100):
+        tiles_to_check = all_neighbors(black_tiles)
+        black_tiles = next_state(tiles_to_check, black_tiles)
+
+    print(f"{len(black_tiles)} tiles will be black after 100 days.")
 
 
 if __name__ == "__main__":
